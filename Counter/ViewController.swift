5ec0e38bcbf0186dfdfc169d331c30cee5ee1a85
Daniil Isakov <serverpgig@yandex.ru>
2025-03-02 21:21:52 +0300

    Add plus and minus buttons

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,16 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
     @IBOutlet weak var counterLabel: UILabel!
-    
     @IBOutlet weak var mainButton: UIButton!
+    
     private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         counterLabel.text = "Значение счётчика:\n\(counter)"
     }
 
-    @IBAction func buttonTouchUp(_ sender: Any) {
+    @IBAction func mainButtonTouchUp(_ sender: Any) {
         counter += 1
         counterLabel.text = "Значение счётчика:\n\(counter)"
     }
